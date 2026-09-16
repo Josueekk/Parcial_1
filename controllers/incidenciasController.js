7945f671dac8451072fdfde6d3db485b41f8e5a1
Josueekk <00017125@uca.edu.sv>
2026-09-16 14:37:03 -0600

    :sparkles: Definicion de metodos en el controller

--- controllers/incidenciasController.js
+++ controllers/incidenciasController.js
@@ -78,7 +78,7 @@ const obtenerIncidencias = (req, res) => {
 // Parametro: id(numerico) */
 const obtenerIncidenciaPorId = (req, res) => {
 
-    // Extraemos el parametro de la URL
+    // Extraemos el parametro de la URL y lo convertimos
     const id = Number(req.params.id);
     // Buscamos el elemento con el metodo find()
     const incidencia = incidencias.find((item) => item.id === id);
@@ -97,21 +97,104 @@ const obtenerIncidenciaPorId = (req, res) => {
 // PUT /Incidencias/:id/estado
 // Parametro: estado */
 const cambiarEstadoIncidencia = (req, res) => {
-    
+    // Extraemos el id para convertirlo
+    const id = Number(req.params.id);
+    // Definimos el estado
+    const { estado } = req.body;
+
+    // Validacion de campo
+    if (!estado) {
+        return res.status(400).json({ mensaje: "El campo es obligatorio" })
+    }
+
+    // Buscamos la incidencia por medio del id con find()
+    const estadoLimpio = limpiarTexto(estado).toLowerCase();
+    let nuevoEstado = "";
+
+    // Validación de estados
+    switch (estadoLimpio) {
+        case "pendiente":
+            nuevoEstado = "Pendiente";
+            break;
+        case "en proceso":
+            nuevoEstado = "En Proceso";
+            break;
+        case "resuelta":
+            nuevoEstado = "Resuelta";
+            break;
+        case "cancelada":
+            nuevoEstado = "Cancelada";
+            break;
+        default:
+            return res.status(400).json({
+                mensaje: "Estado no válido. Opciones permitidas: Pendiente, En Proceso, Resuelta, Cancelada"
+            });
+    }
+    // Mandamos el nuevo estado
+    incidencia.estado = nuevoEstado;
+
+    // Mensaje de exito
+    return res.status(200).json({
+        mensaje: "Estado de incidencia actualizado correctamente", incidencia
+    })
 };
 
 /* -- ENDPOINT 5: Elimar Incidencia -- //
 // DELETE /incidencia/:id */
-const elimarIncidencia = (req, res) => {
-    res.status(501).json({ mensaje: "Por implementar" });
+const eliminarIncidencia = (req, res) => {
+    // Obtenemos el parametro del ID y lo convertimos
+    const id = Number(req.params.id);
+
+    // Localizamos el indice mediante findIndex()
+    const index = incidencias.findIndex((item) => item.id === id);
+
+    // Si no existe
+    if (index === -1) {
+        return res.status(404).json({ mensaje: "Incidencia no encontrada." })
+    }
+
+    // Eliminamos el elemento con splice()
+    incidencias.splice(index, 1);
+
+    // Mensaje de exito
+    return res.status(200).json({ mensaje: "Incidencia eliminada." });
 };
 
 /* -- ENDPOINT 6: Estadisticas  -- //
 // GET / Estadisticas  
 // Retorna: totalIncidencias, pendientes, enProceso, resueltas, canceladas */
 const obtenerEstadisticas = (req, res) => {
-
-    res.status(501).json({ mensaje: "Por implementar" });
+    // Calculamos sin variables contadoras manuales usando reduce()
+    const estadistica = incidencia.reduce(
+        (acumulador, actual) => {
+            acumulador.totalIncidencias++;
+            switch (actual.estado) {
+                case "Pendiente":
+                    acumulador.pendientes++;
+                    break;
+                case "En Proceso":
+                    acumulador.enProceso++;
+                    break;
+                case "Resuelta":
+                    acumulador.resueltas++;
+                    break;
+                case "Cancelada":
+                    acumulador.canceladas++;
+                    break;
+            }
+            return acumulador;
+        },
+        {
+            //Estadistica
+            totalIncidencias: 0,
+            pendientes: 0,
+            enProceso: 0,
+            resueltas: 0,
+            canceladas: 0
+        }
+    );
+        // Retornamos la estadistica
+        return res.status(200).json(estadistica);
 };
 
 /* -- ENDPOINT 7: Clasificación Automática  -- //
@@ -127,7 +210,7 @@ module.exports = {
     obtenerIncidencias,
     obtenerIncidenciaPorId,
     cambiarEstadoIncidencia,
-    //eliminarIncidencia,
+    eliminarIncidencia,
     obtenerEstadisticas,
     obtenerClasificacion
 };
